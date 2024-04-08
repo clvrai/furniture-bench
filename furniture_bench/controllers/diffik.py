9208caf3da830bd825289fe3964c2bc90739ea4c
ankile <lars.ankile@gmail.com>
2024-04-08 14:13:31 -0400

    Start tuning the diffik parameters

--- furniture_bench/controllers/diffik.py
+++ furniture_bench/controllers/diffik.py
@@ -30,8 +30,8 @@ def diffik_factory(real_robot=True, *args, **kwargs):
             self.ee_pos_error = None
             self.ee_rot_error = None
 
-            self.pos_scalar = 4.0
-            self.rot_scalar = 9.0
+            self.pos_scalar = 4.0 / 2.0
+            self.rot_scalar = 9.0 / 4.0
 
             self.scale_errors = True
 
