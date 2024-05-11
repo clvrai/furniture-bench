74bfccdcebf9caad08115c2389862e81c180479c
minoring <minho1350@gmail.com>
2024-05-12 00:33:48 +0900

    add load embedding

--- furniture_bench/envs/initialization_mode.py
+++ furniture_bench/envs/initialization_mode.py
@@ -31,3 +31,53 @@ def str_to_enum(v):
         return Randomness.SKILL_RANDOM
     else:
         raise ValueError(f"Unknown initialization mode: {v}")
+
+def load_embedding(encoder_type, device_id):
+    if encoder_type == "r3m":
+        from r3m import load_r3m
+
+        img_emb_layer = load_r3m("resnet50").module
+        embedding_dim = 2048
+    elif encoder_type == "vip":
+        from vip import load_vip
+
+        model = load_vip().module
+        embedding_dim = 1024
+    elif encoder_type == "liv":
+        from liv import load_liv
+
+        model = load_liv().module
+        embedding_dim = 1024
+    elif encoder_type.startswith("clip"):
+        import clip
+
+        if encoder_type == "clip_vit_b16":
+            model, _ = clip.load("ViT-B/16")
+            embedding_dim = 512
+        if encoder_type == "clip_vit_l14":
+            model, _ = clip.load("ViT-L/14")
+            embedding_dim = 768
+    else:
+        raise ValueError(f"Unknown encoder type: {encoder_type}")
+
+    model.requires_grad_(False)
+    model.eval()
+    model = model.to(device_id)
+
+    if encoder_type.startswith("clip"):
+        import torchvision.transforms as T
+
+        transform = T.Compose(
+            [
+                T.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+            ]
+        )
+
+        def img_emb_layer(x):
+            return model.encode_image(transform(x / 255.0))
+    elif encoder_type in ["liv"]:
+
+        def img_emb_layer(x):
+            return model(x / 255.0)
+
+    return img_emb_layer, embedding_dim
