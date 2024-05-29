b958d051dea7db69240a1b739742958ff9456530
minoring <minho1350@gmail.com>
2024-05-29 21:59:42 +0900

    log learned reward during evaluation

--- implicit_q_learning/evaluation.py
+++ implicit_q_learning/evaluation.py
@@ -6,10 +6,13 @@ import numpy as np
 
 from tqdm import tqdm
 
+from dataset_utils import min_max_normalize, max_normalize, replay_chunk_to_seq
+
 
 def evaluate(
-    agent: nn.Module, env: gym.Env, num_episodes: int, temperature: float = 0.00, log_video: bool = False
+    agent: nn.Module, env: gym.Env, num_episodes: int, temperature: float = 0.00, log_video: bool = False, reward_model = None, normalization="max", max_rew=None, window_size=None
 ) -> Dict[str, float]:
+    # Hacky import to avoid circular imports.
     # stats = {"return": [], "length": []}
     stats = {}
 
@@ -18,14 +21,24 @@ def evaluate(
     log_videos = []
     
     sum_success = 0
+    learned_sum_rewards = []
     for ep_idx in tqdm(range(num_episodes)):
+        observations = []
+        actions = []
+        rewards = []
+
         observation, done = env.reset(), False
         phase = 0
         ep_video = []
         while not done:
+            if reward_model is not None:
+                observations.append(observation)
             obs_without_rgb = {k: v for k, v in observation.items() if k != 'color_image1' and k != 'color_image2'}
             action = agent.sample_actions(obs_without_rgb, temperature=temperature)
             observation, rew, done, info = env.step(action)
+            if reward_model is not None:
+                actions.append(action)
+                rewards.append(rew)
             phase = max(phase, info['phase'])
             sum_reward += rew
             
@@ -33,6 +46,21 @@ def evaluate(
                 # Save first two episodes.
                 # Make it channel first.
                 ep_video.append(observation['color_image2'])
+
+        if reward_model is not None:
+            # compute reds reward
+            x = {
+                'observations': observations,
+                'actions': actions,
+                'rewards': rewards,
+            }
+            seq = reward_model(replay_chunk_to_seq(x, window_size))
+            rewards = np.asarray([elem[reward_model.PUBLIC_LIKELIHOOD_KEY] for elem in seq])
+            if normalization == "min_max":
+                min_max_normalize(rewards)
+            if normalization == "max":
+                rewards = max_normalize(rewards, max_rew)
+            learned_sum_rewards.append(sum(rewards))
 
         if env.furnitures[0].all_assembled(): # Only single environment.
             sum_success += 1
@@ -46,14 +74,16 @@ def evaluate(
 
         # for k in stats.keys():
         #     stats[k].append(info["episode"][k])
-        print("Sum of rewards: ", sum_reward)
+        print("GT sum of rewards: ", sum_reward)
 
     # print("Sum of rewards: ", sum_reward)
     # for k, v in stats.items():
         # stats[k] = np.mean(v)
-    stats['sum_of_reward'] = sum_reward
+    stats['gt_sum_of_reward'] = sum_reward
     # stats['success_rate'] = sum_reward / num_episodes
     stats['success_rate'] = sum_success / num_episodes
     stats['phase'] = np.mean(log_phases)
+    if reward_model is not None:
+        stats['learned_sum_of_reward'] = np.mean(learned_sum_rewards)
 
     return stats, log_videos
