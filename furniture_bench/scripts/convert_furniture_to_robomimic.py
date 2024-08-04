c2c42556ccc24e2cb83c06c525047ca2052e0b91
minroing <minho1350@gmail.com>
2024-08-04 23:22:18 +0900

    add preprocess

---
+++ furniture_bench/scripts/convert_furniture_to_robomimic.py
@@ -0,0 +1,154 @@
+# Example command:
+# python convert_furniture_to_robosuite.py --data-dir <Path_to_generated_data_directory> --out-file <path_to_hdf5>.hdf5 --next-obs
+
+import os
+import argparse
+import pickle
+import h5py
+import json
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+
+import robomimic.utils.tensor_utils as TensorUtils
+
+ENV_NAME = "FurnitureSim-v0"
+RANDOMNESS = "low"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-dir", type=str, required=True, help="directory to pkl dataset"
+    )
+    parser.add_argument("--out-file", type=str, required=True, help="output file name")
+    parser.add_argument("--rot-6d", action="store_true", help="use 6d rotation")
+    parser.add_argument("--next-obs", action="store_true", help="add next_obs to data")
+    parser.add_argument("--n", type=int)
+    parser.add_argument("--sum_rew", type=int, required=True, help="Specify sum of the reward")
+    parser.add_argument("--repeat", type=int, required=True, help="Repeat the data")
+ 
+    args = parser.parse_args()
+
+    input_files = []
+    for root, dirs, files in os.walk(args.data_dir):
+        for f in files:
+            if f.endswith(".pkl"):
+                input_files.append(os.path.join(root, f))
+    # Sort by names.
+    input_files = sorted(input_files)
+    if args.repeat is not None:
+        input_files = input_files * args.repeat
+    
+    if args.n is not None:
+        input_files = input_files[-args.n:]
+
+    f = h5py.File(args.out_file, "a")  # Write mode
+
+    data = pickle.load(open(input_files[0], "rb"))  # Load first file
+
+    # Create "data" group.
+    data_group = f.create_group("data")
+    data_group.attrs["env"] = ENV_NAME
+    data_group.attrs["env_args"] = json.dumps(
+        {
+            "furniture": data["furniture"],
+            "env_name": ENV_NAME,
+            "env_kwargs": {"furniture": data["furniture"]},
+        }
+    )
+    data_group.attrs["env_meta"] = json.dumps(
+        {
+            "env_name": ENV_NAME,
+            "env_kwargs": {"furniture": data["furniture"]},
+        }
+    )
+
+    data_group.attrs["env_info"] = json.dumps({"env_name": ENV_NAME})
+
+    total_samples = 0
+    for demo_idx, input_file in enumerate(tqdm(input_files)):
+        with open(input_file, "rb") as f:
+            data = pickle.load(f)
+
+            # Create demo group.
+            demo_group = data_group.create_group(f"demo_{demo_idx}")
+            num_samples = len(data["actions"])  # number of samples in this demo
+            demo_group.attrs["num_samples"] = num_samples
+
+            demo_group.create_dataset("actions", data=np.array(data["actions"]))
+
+            if sum(data['skills']) != args.sum_rew:
+                import pdb; pdb.set_trace()
+                print(f"Demo {demo_idx} does not have all skills.")
+            # if sum(data['rewards']) != 1:
+            #     import pdb; pdb.set_trace()
+            
+            # Convert last reward to the larger value.
+            # Do not use the reward.
+            rewards = np.array(data['rewards'])
+            rewards = np.where(rewards == 1, 0, rewards)
+            assert sum(rewards) == 0
+            
+            skills = np.array(data['skills'])
+            idxs = np.where(skills == 1)[0]
+            assert len(idxs) == args.sum_rew
+            # for rew, idx in zip(range(1, 17), idxs):
+            #     rewards[idx] = rew
+            rewards[idxs] = 1
+            # assert sum(rewards) == sum(range(1, 17))
+            assert sum(rewards) == args.sum_rew
+            demo_group.create_dataset("rewards", data=np.array(rewards))
+            # Create dones data with length of rewards, but last element 1.
+            dones = np.concatenate([np.zeros_like(data["rewards"]), np.ones(1)])
+            demo_group.create_dataset("dones", data=dones)
+
+            # Extract robot_state keys.
+            robot_state_keys = list(data["observations"][0]["robot_state"].keys())
+            for obs in data["observations"]:
+                for k in robot_state_keys:
+                    obs[k] = obs["robot_state"][k]
+                del obs["robot_state"]
+
+            obs = TensorUtils.list_of_flat_dict_to_dict_of_list(data["observations"])
+
+            # Change the key of the data["observations"]
+            # "parts_poses"   -> "object"
+            # "ee_pos"        -> "robot0_eef_pos"
+            # "ee_quat"       -> "robot0_eef_quat"
+            # "gripper_width" -> "robot0_gripper_qpos"
+            # "color_image1"  -> "robot0_eye_in_hand_image"
+            # "color_image2"  -> "agentview_image"
+            # obs["object"] = obs["parts_poses"]
+            obs["robot0_eef_pos"] = obs["ee_pos"]
+            obs["robot0_eef_quat"] = obs["ee_quat"]
+            obs["robot0_gripper_qpos"] = np.array(obs["gripper_width"]).reshape(-1, 1) # Add a dimension since squeezed in data saving.
+            obs["robot0_eye_in_hand_image"] = obs["color_image1"]
+            obs["agentview_image"] = obs["color_image2"]
+
+            # del obs["parts_poses"]
+            del obs["ee_pos"]
+            del obs["ee_quat"]
+            del obs["gripper_width"]
+            del obs["color_image1"]
+            del obs["color_image2"]
+
+            for k in obs:
+                demo_group.create_dataset("obs/{}".format(k), data=np.array(obs[k]))
+
+                if args.next_obs:
+                    demo_group.create_dataset(
+                        "next_obs/{}".format(k), data=np.array(obs[k])[1:]
+                    )
+
+            total_samples += num_samples
+
+    data_group.attrs["total"] = total_samples
+
+
+if __name__ == "__main__":
+    main()
+
+
--- robomimic/robomimic/scripts/conversion/extract_feature.py
+++ robomimic/robomimic/scripts/conversion/extract_feature.py
@@ -5,11 +5,30 @@ import numpy as np
 
 # import torchvision.transforms as T
 from torchvision.transforms import v2
+import kornia.augmentation as K
 
 
 import torch
 from tqdm import tqdm
 from PIL import Image
+
+
+class AttrDict(dict):
+    __setattr__ = dict.__setitem__
+
+    def __getattr__(self, attr):
+        # Take care that getattr() raises AttributeError, not KeyError.
+        # Required e.g. for hasattr(), deepcopy and OrderedDict.
+        try:
+            return self.__getitem__(attr)
+        except KeyError:
+            raise AttributeError("Attribute %r not found" % attr)
+
+    def __getstate__(self):
+        return self
+
+    def __setstate__(self, d):
+        self = d
 
 
 def feature_extractor_factory(feature_extractor):
@@ -42,10 +61,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data-dir", type=str, help="directory to hdf5 dataset")
     parser.add_argument("--dataset", type=str, help="dataset name")
+    parser.add_argument("--random_noise", action='store_true')
 
     parser.add_argument(
         "--feature-extractor", type=str, required=True, choices=["r3m", "liv"]
     )
+    parser.add_argument("--n_demos", type=int, help="number_of_demos", required=True)
     args = parser.parse_args()
 
     device = "cuda"
@@ -60,11 +81,67 @@ if __name__ == "__main__":
 
     feature_extractor, transforms = feature_extractor_factory(args.feature_extractor)
 
+
+    if args.random_noise:
+        aug_cfg = AttrDict(
+        # Taken from LIBERO/code for TAIL: Task specfic adapters for imitation learning
+                brightness=0.1,
+                contrast=0.1,
+                saturation=0.1,
+                hue=0.1,
+                color_p=0.9,
+                noise_std=0.1,
+                noise_p=0.0,
+                channel_shuffle_p=0.0,
+                degrees=5,
+                translate=0.05,
+                affine_p=0.6,
+                erase_p=0.,
+            )
+        image_aug_fn_front = torch.nn.Sequential(
+            K.ColorJitter(
+                aug_cfg.brightness,
+                aug_cfg.contrast,
+                aug_cfg.saturation,
+                aug_cfg.hue,
+                p=aug_cfg.color_p,
+            ),
+            K.RandomGaussianNoise(std=aug_cfg.noise_std, p=aug_cfg.noise_p),
+            K.RandomChannelShuffle(p=aug_cfg.channel_shuffle_p),
+            K.RandomAffine(
+                degrees=aug_cfg.degrees,
+                translate=(aug_cfg.translate, aug_cfg.translate),
+            ),
+            # K.RandomErasing(p=aug_cfg.erase_p),
+            # K.Normalize(
+            #    mean=image_processor.image_mean, std=image_processor.image_std
+            # ),
+        )
+        image_aug_fn_wrist = torch.nn.Sequential(
+            K.ColorJitter(
+                aug_cfg.brightness,
+                aug_cfg.contrast,
+                aug_cfg.saturation,
+                aug_cfg.hue,
+                p=aug_cfg.color_p,
+            ),
+            K.RandomGaussianNoise(std=aug_cfg.noise_std, p=aug_cfg.noise_p),
+            K.RandomChannelShuffle(p=aug_cfg.channel_shuffle_p),
+            # K.RandomAffine(
+            #     degrees=aug_cfg.degrees,
+            #     translate=(aug_cfg.translate, aug_cfg.translate),
+            # ),
+            # K.RandomErasing(p=aug_cfg.erase_p),
+            # K.Normalize(
+            #    mean=image_processor.image_mean, std=image_processor.image_std
+            # ),
+        )
+    
     batch_size = 512
     for dataset in input_files:
         with h5py.File(dataset, "a") as f:
             demos = sorted(list(f["data"].keys()), key=lambda x: int(x[5:]))
-            for ep in tqdm(demos):
+            for aug_idx, ep in enumerate(tqdm(demos)):
                 for k in f["data/{}".format(ep)]:
                     if k in ["obs", "next_obs"]:
                         for obs_k in f["data/{}/{}".format(ep, k)]:
@@ -79,7 +156,7 @@ if __name__ == "__main__":
                                 all_embeddings = (
                                     []
                                 )  # List to accumulate batch embeddings
-
+                                
                                 # Process in minibatches
                                 for i in range(0, len(input_img), batch_size):
                                     batch_imgs = input_img[i : i + batch_size]
@@ -87,6 +164,17 @@ if __name__ == "__main__":
                                         torch.from_numpy(batch_imgs.astype(np.uint8))
                                     )
                                     preprocessed_batch = preprocessed_batch.to(device)
+                                    augmented = False
+                                    if args.random_noise and aug_idx > args.n_demos:
+                                        preprocessed_batch = preprocessed_batch / 255. # [0, 1] range.
+                                        augmented = True
+                                        if obs_k == "agentview_image":
+                                            preprocessed_batch = image_aug_fn_front(preprocessed_batch)
+                                        elif obs_k == "robot0_eye_in_hand_image":
+                                            preprocessed_batch = image_aug_fn_wrist(preprocessed_batch)
+                                    if augmented:
+                                        preprocessed_batch = preprocessed_batch * 255.
+
 
                                     with torch.no_grad():
                                         batch_embedding = (
